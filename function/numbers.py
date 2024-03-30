af424c1c17e6bead401acfd9292b48ad1c5bd330
msanjith130@gmail.com <148097204+sanjithhithub@users.noreply.github.com>
2024-03-30 19:07:48 +0530

    first commit

---
+++ function/numbers.py
@@ -0,0 +1,5 @@
+number1 = int(input("enter the number1:"))
+number2 = int(input("enter the number2:"))
+c = number1 + number2  , number1 - number2 , number1 * number2, number1 / number2
+
+print("the total number:",c)
